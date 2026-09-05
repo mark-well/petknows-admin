efa4ca5211a36f1ff646b10935570ab926eacf75
mark-well <lacemerto05@gmail.com>
2026-09-05 20:54:52 +0800

    ref: return pet by latest date of registration

--- src/features/pet-management/services/getUserPets.ts
+++ src/features/pet-management/services/getUserPets.ts
@@ -2,10 +2,13 @@ import { supabase } from "../../../utils/supabase";
 import { queryPet } from "../api/queries";
 
 export default async function getUserPets(userId: string | null) {
-  if(!userId) throw new Error("No user id provided");
-  
-  const {data, error} = await supabase.from("pets").select(queryPet).eq("user_id", userId);
+  if (!userId) throw new Error("No user id provided");
 
-  if(error) throw error;
+  const { data, error } = await supabase.from("pets").select(queryPet).eq(
+    "user_id",
+    userId,
+  ).order("created_at", { ascending: false });
+
+  if (error) throw error;
   return data;
 }
